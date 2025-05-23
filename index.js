900a0d717594f2a1870b95ac649ea26815005d90
Daniel Ransdorf <daniel.ransdorf@proton.me>
2025-05-23 13:23:37 +0200

    Details

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const navToggle = document.getElementById('nav-toggle');
-  const mainNav = document.getElementById('main-nav');
+document.addEventListener("DOMContentLoaded", () => {
+  const navToggle = document.getElementById("nav-toggle");
+  const mainNav = document.getElementById("main-nav");
 
   if (navToggle && mainNav) {
-    navToggle.addEventListener('click', () => {
-      const isOpen = mainNav.classList.toggle('open');
-      navToggle.classList.toggle('open', isOpen);
-      navToggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
+    navToggle.addEventListener("click", () => {
+      const isOpen = mainNav.classList.toggle("open");
+      navToggle.classList.toggle("open", isOpen);
+    });
+    mainNav.addEventListener("click", () => {
+      console.log("click");
+      mainNav.classList.remove("open");
+      navToggle.setAttribute("aria-expanded", "false");
     });
   }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    let currentYear = new Date().getFullYear();
-    let copyrighYearElement = document.getElementById("copyright-year");
-    copyrighYearElement.innerHTML = currentYear;
-})
+  let currentYear = new Date().getFullYear();
+  let copyrighYearElement = document.getElementById("copyright-year");
+  copyrighYearElement.innerHTML = currentYear;
+});
